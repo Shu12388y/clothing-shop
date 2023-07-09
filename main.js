707df9fadb056ea87dbdc00b0481a56fb45a498d
Shu12388y <spl.sp999@gmail.com>
2023-07-09 21:02:21 +0530

    file added

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-import 'animate.css';
+// import "animate.css";
+window.addEventListener("mousemove", function (e) {
+  const cursor = document.querySelector(".mouse");
+  const x_position = e.clientX;
+  const y_position = e.clientY;
+  console.log(x_position, y_position);
+  cursor.style.top = y_position + "px";
+  cursor.style.left = x_position + "px";
+});
